684b7e5deb8ee65335d901e16d289b5078aca062
asma1mokeddes <amokeddes@myges.fr>
2024-03-26 17:13:38 +0100

    feat(generator-list): Adding list shopping generator

--- back/controllers/shoppingList.js
+++ back/controllers/shoppingList.js
@@ -1,25 +1,24 @@
 const OpenAI = require("openai");
 
-exports.chatWithChef = async (req, res, next) => {
-    const { message } = req.body;
+exports.generateIntelligentShoppingList = async (req, res) => {
+    const { shoppingList } = req.body;
+    const prompt = `Liste les ingrédients nécessaires pour une recette contenant : ${shoppingList}. Fournis une liste claire et concise, incluant des quantités estimées et des recommandations de produits.`;
     const openai = new OpenAI(process.env.OPENAI_API_KEY);
-
     async function main() {
         const completion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [
                 {
                     role: "system",
-                    content: `Tu es un chef étoilé au guide Michelin avec une quinzaine d'années d'expérience dans le métier, ayant gagné plusieurs concours culinaires à l'international. Réponds aux questions des utilisateurs avec ton expertise.`,
+                    content: "Tu es un chef étoilé...",
                 },
                 {
                     role: "user",
-                    content: message,
+                    content: prompt,
                 },
             ],
         });
         res.json({ response: completion.choices[0].message.content });
     }
-
     main().catch((err) => res.status(500).json({ error: err.message }));
 };
--- back/index.js
+++ back/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 const express = require("express");
@@ -11,6 +11,8 @@ const recipeRouter = require("./routes/recipe.js");
 const categoryRouter = require("./routes/category.js");
 const assistantRouter = require("./routes/assistant.js");
 const chatRouter = require("./routes/chatbot.js");
+const shoppingListRouter = require("./routes/shoppingList.js");
+
 const app = express();
 
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -18,8 +20,8 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
 app.use(express.json());
 const corsOptions = {
-  origin: process.env.FRONT_URL, // Remplacez par l'URL de votre frontend
-  credentials: true,
+    origin: process.env.FRONT_URL, // Remplacez par l'URL de votre frontend
+    credentials: true,
 };
 app.use(cors(corsOptions));
 app.use("/auth", authRouter);
@@ -28,25 +30,26 @@ app.use("/recipe", recipeRouter);
 app.use("/category", categoryRouter);
 app.use("/assistant", assistantRouter);
 app.use("/chat", chatRouter);
+app.use("/list", shoppingListRouter);
 
 app.use((error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message: message, invalid_data: data });
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, invalid_data: data });
 });
 
 const startServer = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI);
-    console.log("Connected to database");
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        await mongoose.connect(process.env.DB_URI);
+        console.log("Connected to database");
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 startServer();
 
 app.listen(port || 3000, () => {
-  console.log(`Cuisine-Connect app listening on port ${port}`);
+    console.log(`Cuisine-Connect app listening on port ${port}`);
 });
