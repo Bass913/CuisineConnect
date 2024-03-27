08aa8c119adc372adc0ecde0c640ae4717eb327a
Rafael93150 <tavaresrafael93@gmail.com>
2024-03-27 10:35:33 +0100

    redisign searchbar

--- front/src/components/Button.jsx
+++ front/src/components/Button.jsx
@@ -1,19 +1,7 @@
-export default function Button({ onClick, backgroundColor, children, type }) {
-    const buttonStyle = {
-        height: "2.5rem",
-        padding: "1.75rem",
-        fontWeight: "bold",
-        display: "flex",
-        alignItems: "center",
-        border: "1px solid",
-        borderRadius: "0.25rem",
-        backgroundColor: backgroundColor || "transparent",
-        color: backgroundColor ? "#ffffff" : "#000000",
-    };
-
-    return (
-        <button style={buttonStyle} onClick={onClick} type={type}>
-            {children}
-        </button>
-    );
+export default function Button({ onClick, className, children, type }) {
+	return (
+		<button onClick={onClick} type={type} className={className}>
+			{children}
+		</button>
+	);
 }
