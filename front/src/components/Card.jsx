2fa63620faab884b0c3fa187233b5b19bd3ff6d1
Bass913 <bastiendikiadi@outlook.fr>
2024-03-28 14:53:03 +0100

    Heart Icon cursor fixed

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -3,10 +3,9 @@ import { NavLink } from "react-router-dom";
 import slugify from "react-slugify";
 import Favorite from "./Favorite";
 
-
 export default function Card({ recipe }) {
   return (
-    <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+    <div className="flex flex-col justify-between max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
       <img
         src={recipe.img}
         alt=""
@@ -14,8 +13,7 @@ export default function Card({ recipe }) {
       />
       <div className="flex flex-col justify-between p-6 space-y-8">
         <div className="space-y-2">
-          <h2 className="text-3xl font-semibold tracki">{recipe.title}</h2>
-
+          <h2 className=" text-2xl font-semibold tracki">{recipe.title}</h2>
         </div>
         <NavLink
           to={`/recipe/${slugify(recipe.title)}`}
--- front/src/components/LastRecipes.jsx
+++ front/src/components/LastRecipes.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { getRecipes } from '../api/recipe';
-import Card from './Card';
+import React, { useEffect, useState } from "react";
+import { getRecipes } from "../api/recipe";
+import Card from "./Card";
 
 export default function LastRecipes() {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        const fetchRecipes = async () => {
-            const recipes = await getRecipes();
-            setRecipes(recipes);
-        };
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const recipes = await getRecipes();
+      setRecipes(recipes);
+    };
 
-        fetchRecipes();
-    }, []);
+    fetchRecipes();
+  }, []);
 
-    return (
-        <>
-            <div className="flex gap-5 mt-10">
-                {recipes.map((recipe) => (
-                    <Card key={recipe._id} recipe={recipe} />
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex  gap-5 mt-10">
+        {recipes.map((recipe) => (
+          <Card key={recipe._id} recipe={recipe} />
+        ))}
+      </div>
+    </>
+  );
 }
