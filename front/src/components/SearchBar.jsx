7299c10922a2ec6088504786d054afd768d69a54
asma1mokeddes <amokeddes@myges.fr>
2024-03-28 16:07:24 +0100

    fix search

--- front/src/components/SearchBar.jsx
+++ front/src/components/SearchBar.jsx
@@ -1,82 +1,72 @@
 import { useState, useEffect } from "react";
-import { Form } from "react-router-dom";
 import Button from "./Button.jsx";
 import {
-	MagnifyingGlassIcon,
-	MicrophoneIcon,
+  MagnifyingGlassIcon,
+  MicrophoneIcon,
 } from "@heroicons/react/24/outline";
 import useSpeechToText from "../hooks/useSpeechToText";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = ({ initialValue }) => {
-	const [search, setSearch] = useState(initialValue);
-	const navigate = useNavigate();
-	const { isListening, transcript, startListening, stopListening } =
-		useSpeechToText({ continuous: true });
+  const [search, setSearch] = useState(initialValue);
+  const navigate = useNavigate();
+  const { isListening, transcript, startListening, stopListening } =
+    useSpeechToText({ continuous: true });
 
-	const startStopListening = () => {
-		console.log("isListening valeur dans VoiceInput", isListening);
-		isListening ? stopVoiceInput() : startListening();
-	};
+  const startStopListening = () => {
+    console.log("isListening valeur dans VoiceInput", isListening);
+    isListening ? stopVoiceInput() : startListening();
+  };
 
-	const stopVoiceInput = () => {
-		setSearch(
-			(preVal) =>
-				preVal +
-				(transcript.length
-					? (preVal.length ? " " : "") + transcript
-					: "")
-		);
-		stopListening();
-	};
+  const stopVoiceInput = () => {
+    setSearch(
+      (preVal) =>
+        preVal +
+        (transcript.length ? (preVal.length ? " " : "") + transcript : "")
+    );
+    stopListening();
+  };
 
-	useEffect(() => {
-		setSearch(initialValue);
-	}, [initialValue]);
+  useEffect(() => {
+    setSearch(initialValue);
+  }, [initialValue]);
 
-	const handleChange = (e) => {
-		setSearch(e.target.value);
-	};
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-	const handleSubmit = () => {
-		if (!search) {
-			navigate("/");
-			return;
-		}
-		navigate(`/?search=${search}`);
-	};
-	return (
-		<div className="flex items-center justify-center mt-28 relative w-full md:w-128">
-			<MagnifyingGlassIcon className="w-5 h-5 text-gray-600 absolute  left-3 z-10" />
-			<input
-				type="text"
-				aria-label="search recipes"
-				className="w-full h-10 p-7 border border-gray-600 rounded focus:border-rose-600 focus:outline-none relative pl-10 pr-20 hover:border-rose-600 hover:bg-gray-50 text-sm text-gray-600 font-normal placeholder-gray-500 transition-all duration-200"
-				placeholder="Rechercher une recette"
-				name="search"
-				disabled={isListening}
-				value={
-					isListening
-						? search +
-						  (transcript.length
-								? (search.length ? " " : "") + transcript
-								: "")
-						: search
-				}
-				onChange={handleChange}
-			/>
-			<Button
-				className="text-white font-normal bg-rose-600 hover:bg-rose-700 h-10 rounded p-3 ml-2 absolute right-2 z-10 flex items-center text-sm"
-				onClick={handleSubmit}
-			>
-				Rechercher
-			</Button>
-			<MicrophoneIcon
-				onClick={() => startStopListening()}
-				className="w-5 h-5 cursor-pointer text-gray-600 absolute right-28 z-10"
-			/>{" "}
-		</div>
-	);
+  const handleSubmit = () => {
+    if (!search) {
+      navigate("/");
+      return;
+    }
+    navigate(`/?search=${search}`);
+  };
+  return (
+    <div className="flex items-center justify-center mt-28 relative w-full md:w-128">
+      <MagnifyingGlassIcon className="w-5 h-5 text-gray-600 absolute  left-3 z-10" />
+      <input
+        type="text"
+        aria-label="search recipes"
+        className="w-full h-10 p-7 border border-gray-600 rounded focus:border-rose-600 focus:outline-none relative pl-10 pr-20 hover:border-rose-600 hover:bg-gray-50 text-sm text-gray-600 font-normal placeholder-gray-500 transition-all duration-200"
+        placeholder="Rechercher une recette"
+        name="search"
+        value={transcript}
+        onChange={handleChange}
+        disabled={isListening}
+      />
+      <Button
+        className="text-white font-normal bg-rose-600 hover:bg-rose-700 h-10 rounded p-3 ml-2 absolute right-2 z-10 flex items-center text-sm"
+        onClick={handleSubmit}
+      >
+        Rechercher
+      </Button>
+      <MicrophoneIcon
+        onClick={() => startStopListening()}
+        className="w-5 h-5 cursor-pointer text-gray-600 absolute right-28 z-10"
+      />{" "}
+    </div>
+  );
 };
 
 export default SearchBar;
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import Root from "./layouts/root.jsx";
 import ErrorPage from "./pages/error-page.jsx";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider, defer } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RecipeDetail from "./pages/recipe-detail.jsx";
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
@@ -13,49 +13,61 @@ import ProtectedRoute from "./middlewares/ProtectedRoute.jsx";
 import FavoriteRecipes from "./pages/favorite-recipes.jsx";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Root />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: "",
-				element: <App />,
-			},
-			{
-				path: "auth",
-				children: [
-					{
-						path: "login",
-						element: <Login />,
-					},
-					{
-						path: "register",
-						element: <Register />,
-					},
-				],
-			},
-			{
-				path: "recipe/:title",
-				element: <RecipeDetail />,
-			},
-			{
-				path: "profile",
-				children: [
-					{
-						path: "favorite-recipes",
-						element: <FavoriteRecipes />,
-					},
-				],
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <App />,
+      },
+
+      {
+        path: "auth",
+        children: [
+          {
+            path: "",
+            element: <App />,
+          },
+          {
+            path: "auth",
+            element: <ProtectedRoute />,
+            children: [
+              {
+                path: "login",
+                element: <Login />,
+              },
+              {
+                path: "register",
+                element: <Register />,
+              },
+            ],
+          },
+
+          {
+            path: "recipe/:title",
+            element: <RecipeDetail />,
+          },
+          {
+            path: "profile",
+            children: [
+              {
+                path: "favorite-recipes",
+                element: <FavoriteRecipes />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<UserProvider>
-			<RouterProvider router={router} />
-		</UserProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  </React.StrictMode>
 );
