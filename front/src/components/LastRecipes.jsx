cb434a1fe6932ef088c6c2eb7b51505be46b9a41
Rafael93150 <tavaresrafael93@gmail.com>
2024-03-28 19:20:40 +0100

    redisign last recipes

--- front/src/components/LastRecipes.jsx
+++ front/src/components/LastRecipes.jsx
@@ -15,12 +15,13 @@ export default function LastRecipes() {
   }, []);
 
   return (
-    <>
-      <div className="flex  gap-5 mt-10">
+	<div className="flex flex-col items-center mx-auto max-w-5xl my-16">
+	  <h1 className="text-xl font-semibold text-center mb-12">Dernières recettes</h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 mx-auto">
         {recipes.map((recipe) => (
           <Card key={recipe._id} recipe={recipe} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
