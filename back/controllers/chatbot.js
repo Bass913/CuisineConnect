994e2008ad0334b3c6d12a290a4c93b43e8de5d8
asma1mokeddes <amokeddes@myges.fr>
2024-04-08 04:39:17 +0200

    feat(streaming): Adding streaming feature

--- back/controllers/chatbot.js
+++ back/controllers/chatbot.js
@@ -8,21 +8,38 @@ let messageHistory = [
 ];
 exports.chatWithChef = async (req, res, next) => {
     const { message } = req.body;
-    const openai = new OpenAI(process.env.OPENAI_API_KEY);
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
     messageHistory.push({
         role: "user",
         content: message,
     });
     async function main() {
-        const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: messageHistory,
-        });
-        messageHistory.push({
-            role: "assistant",
-            content: completion.choices[0].message.content,
-        });
-        res.json({ response: completion.choices[0].message.content });
+        try {
+            let fullText = "";
+
+            const completion = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages: messageHistory,
+                stream: true,
+            });
+
+            for await (const part of completion) {
+                let text = part.choices[0].delta.content ?? "";
+                fullText += text;
+                res.write(`${text}`);
+            }
+        } catch (err) {
+            console.error("Erreur lors de la communication avec OpenAI:", err);
+            res.status(500).write(
+                `data: ${JSON.stringify({ error: err.message })}\n\n`
+            );
+        } finally {
+            res.end();
+        }
     }
 
     main().catch((err) => res.status(500).json({ error: err.message }));
