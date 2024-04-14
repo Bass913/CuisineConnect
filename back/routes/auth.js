316a2f6012a368a3ec633207dff2320e765fb2f0
asma1mokeddes <amokeddes@myges.fr>
2024-04-14 17:34:25 +0200

    fix logout

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { register, login, verify, logout } = require("../controllers/auth.js");
-
+const { isAuth } = require("../middlewares/isAuth.js");
 const router = express.Router();
 
 router.post("/register", register);
 router.get("/verify/:token", verify);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", isAuth, logout);
 
 module.exports = router;
